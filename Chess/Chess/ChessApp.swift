ed83b25584e831dea92317ab276cbfd2b7c39efb
Yash Sangha <yashpalsangha@gmail.com>
2022-11-02 17:27:34 +0000

    Created Xcode SwiftUI project named Chess

---
+++ Chess/Chess/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  Chess
+//
+//  Created by Yash Sangha on 02/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
